3fb8e7711766b6e4837d81417e1f67210a4918cf
Eric Wolff <ericwolff42@gmail.com>
2024-03-22 16:24:00 -0400

    Added timeline element

--- js/main.js
+++ js/main.js
@@ -6,15 +6,18 @@ d3.csv('data/ufo_sightings.csv')
     data.forEach(d => {
       d.latitude = +d.latitude; //make sure these are not strings
       d.longitude = +d.longitude; //make sure these are not strings
-      d.year = new Date(d.date_time).getFullYear();
-      d.month = new Date(d.date_time).getMonth() + 1;
-
       d.date_time = new Date(d.date_time)
+      d.year = d.date_time.getFullYear();
+      d.month = d.date_time.getMonth() + 1;
       d.time = d.date_time.getHours() + d.date_time.getMinutes()/60
     });
-
+    const yearlyFrequency = Array.from(d3.rollup(data, v => v.length, d => d.year), ([year, frequency]) => ({year, frequency}));
+    yearlyFrequency.sort((a, b) => a.year - b.year);
     // Initialize chart and then show it
     leafletMap = new LeafletMap({ parentElement: '#my-map'}, data);
+
+    // Initialize chart and then show it
+    timeline = new TimeLine({ parentElement: '#timeline'}, yearlyFrequency);
 
     d3.select(`#color_attr`).on('change', function() {
       const selectedOption = d3.select(this).property('value');
@@ -23,7 +26,7 @@ d3.csv('data/ufo_sightings.csv')
       leafletMap.updateColors(selectedOption);
     });
     leafletMap.updateColors("year");
-    
+    timeline.updateVis();
   })
   .catch(error => console.error(error));
   
